d9f8459e94a9312124a69505a43885196a50fb47
Harika0505 <harika.dharmapuri@elait.com>
2025-09-16 14:39:04 +0530

    orders_without_customers.sql

---
+++ Jaffle_Shop/data-tests/orders_without_customers.sql
@@ -0,0 +1,6 @@
+-- Fail if any order refers to a non-existing customer
+select o.order_id, o.customer_id
+from {{ ref('stg_orders') }} o
+left join {{ ref('stg_customers') }} c
+  on o.customer_id = c.customer_id
+where c.customer_id is null
