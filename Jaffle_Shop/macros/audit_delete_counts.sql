d3c626fa3c810b5fd4405b2af048a880cffa3d72
Shajahan-Hussain <harika.dharmapuri@elait.com>
2025-09-23 11:51:20 +0000

    Modified macros with arguments - latest

--- Jaffle_Shop/macros/audit_delete_counts.sql
+++ Jaffle_Shop/macros/audit_delete_counts.sql
@@ -1,19 +1,20 @@
-{% test audit_delete_counts(model, raw_table, key_column) %}
+{% test audit_delete_counts(model, raw_table, src_key_column, stg_key_column, is_deleted_column) %}
 
 with raw_deleted as (
-  select count(distinct {{ key_column }}) as raw_cnt
+  select count(distinct {{ src_key_column }}) as raw_cnt
   from {{ raw_table }}
-  where is_deleted = true
+  where {{ is_deleted_column }} = true
 ),
 
 audit_deleted as (
-  select count(distinct customer_id) as audit_cnt
+  select count(distinct {{ stg_key_column }}) as audit_cnt
   from {{ model }}
   where audit_action = 'DELETE'
 )
 
 select r.raw_cnt, a.audit_cnt
-from raw_deleted r cross join audit_deleted a
+from raw_deleted r
+cross join audit_deleted a
 where r.raw_cnt != a.audit_cnt
 
 {% endtest %}
