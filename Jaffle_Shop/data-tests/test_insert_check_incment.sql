8af0f0594c1504bbaa87f44b249aba71f47d72b6
Shajahan-Hussain <sambit.nayak@elait.com>
2025-09-24 10:55:18 +0000

    All_Test_Passed-24-09-2024

--- Jaffle_Shop/data-tests/test_insert_check_incment.sql
+++ Jaffle_Shop/data-tests/test_insert_check_incment.sql
@@ -23,8 +23,4 @@ SELECT *
        --ordered_at,
        --record_status
 FROM check_missing
-<<<<<<< local
 WHERE record_status != 'Present in All Layers'
-=======
-WHERE record_status != 'Present in All Layers'
->>>>>>> remote
