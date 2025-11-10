cf1c60e5ca3a41af5a1b6bda1a060f113d6711d9
Shajahan-Hussain <roshan.lal@elait.com>
2025-11-10 09:54:07 +0000

    Extra objects

--- Jaffle_Shop/data-tests/orders_without_customers.sql
+++ Jaffle_Shop/data-tests/orders_without_customers.sql
@@ -4,3 +4,5 @@ from {{ ref('stg_orders') }} o
 left join {{ ref('stg_customers') }} c
   on o.customer_id = c.customer_id
 where c.customer_id is null
+
+
--- Jaffle_Shop/data-tests/test_raw_supplies_perishable_values.sql
+++
@@ -1,4 +0,0 @@
-select *
-from {{ source('ecom', 'raw_supplies') }}
-where perishable not in (true, false)
-   or perishable is null
