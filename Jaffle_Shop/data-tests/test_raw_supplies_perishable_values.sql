ba9ddd989c3cddca7b9072a8fd18d3c4ee84e392
Shajahan-Hussain <harika.dharmapuri@elait.com>
2025-11-11 05:36:45 +0000

    added warn test for supplies and freshness

--- Jaffle_Shop/data-tests/test_raw_supplies_perishable_values.sql
+++ Jaffle_Shop/data-tests/test_raw_supplies_perishable_values.sql
@@ -1,4 +1,11 @@
+{{ config(
+    severity='warn',
+    store_failures=true
+) }}
+
+{% do log("⚠️ Warning: Some 'perishable' values are NULL or not TRUE/FALSE in raw_supplies.", info=True) %}
 select *
 from {{ source('ecom', 'raw_supplies') }}
 where perishable not in (true, false)
-   --or perishable is null (For this Test case to pass quoted this line since in raw_supplies it contains null values)
+   or perishable is null 
+   --(For this Test case to pass quoted this line since in raw_supplies it contains null values)
