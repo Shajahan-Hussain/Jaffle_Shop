5a74d4e43474940e3270f75c57a631c24cfbbdd5
Shajahan-Hussain <swetha.palanisamy@elait.com>
2025-12-18 08:21:31 +0000

    deleted file for sync check

--- Jaffle_Shop/macros/order_amount_positive.sql
+++
@@ -1,8 +0,0 @@
-{% test order_amount_positive(model, column_name) %}
--- to check the no negative values in the amount columns
-
-     select *
-     from {{ model }}
-     where {{ column_name }} <= 0
-     
-{% endtest %}
