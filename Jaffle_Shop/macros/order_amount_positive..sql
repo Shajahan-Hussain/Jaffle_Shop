1af1e192eee187ab1fff7f570f73291e42c1a893
Shajahan-Hussain <swetha.palanisamy@elait.com>
2025-12-18 08:47:33 +0000

    Updated comment in test file

--- Jaffle_Shop/macros/order_amount_positive..sql
+++ Jaffle_Shop/macros/order_amount_positive..sql
@@ -1,5 +1,5 @@
 {% test order_amount_positive(model, column_name) %}
--- to test the column has no negative values
+-- This test will validate the amount is posiotive or not
     select *
     from {{ model }}
     where {{ column_name }} <= 0
