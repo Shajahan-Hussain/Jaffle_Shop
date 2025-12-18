848108d028c1f91143e1993c909bf5b0d7a695e9
Shajahan-Hussain <harika.dharmapuri@elait.com>
2025-12-18 06:12:53 +0000

    inc_data_drift test added

--- Jaffle_Shop/macros/incremental_data_drift.sql
+++ Jaffle_Shop/macros/incremental_data_drift.sql
@@ -1,4 +1,4 @@
-{% test incremental_data_drift(model, compare_model, unique_key, updated_at) %}
+{% test incremental_data_drift(model, compare_model, unique_key, updated_at, src_updated_at) %}
 
 with source as (
     select * from {{ compare_model }}
@@ -11,11 +11,11 @@ mismatched as (
     select
         t.{{ unique_key }},
         t.{{ updated_at }} as target_updated,
-        s.{{ updated_at }} as source_updated
+        s.{{ src_updated_at }} as source_updated
     from target t
     join source s
       on t.{{ unique_key }} = s.{{ unique_key }}
-    where t.{{ updated_at }} < s.{{ updated_at }}
+    where t.{{ updated_at }} < s.{{ src_updated_at }}
 )
 
 select * from mismatched
